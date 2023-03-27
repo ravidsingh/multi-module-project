89f6b8fc1c01b3db05c8ffb99f01a36b640e0a82
ravidsingh <125184625+ravidsingh@users.noreply.github.com>
2023-03-27 15:43:46 +0530

    Update MainController.java

--- AdminApp/src/main/java/net/codejava/admin/MainController.java
+++ AdminApp/src/main/java/net/codejava/admin/MainController.java
@@ -8,6 +8,8 @@ import net.codejava.lib.CommonUtil;
 
 @Controller
 public class MainController {
+	
+	//admin app controller
 	@GetMapping("")
 	public String viewHomePage(Model model) {
 		String appName = CommonUtil.getAppName();
