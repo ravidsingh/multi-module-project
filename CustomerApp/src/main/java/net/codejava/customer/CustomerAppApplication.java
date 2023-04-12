af0ced3e2999e58d0d7bea441532042650d61430
ravidsingh <125184625+ravidsingh@users.noreply.github.com>
2023-04-12 20:44:50 +0530

    Revert "IQE-2"

--- CustomerApp/src/main/java/net/codejava/customer/CustomerAppApplication.java
+++ CustomerApp/src/main/java/net/codejava/customer/CustomerAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerAppApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerAppApplication.class, args);
 	}
