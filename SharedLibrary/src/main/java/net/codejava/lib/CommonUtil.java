db95f695808671aeb5da98cef82c0ecf08e619d2
ravidsingh <125184625+ravidsingh@users.noreply.github.com>
2023-04-12 20:35:21 +0530

    Revert "IQE-2"

--- SharedLibrary/src/main/java/net/codejava/lib/CommonUtil.java
+++ SharedLibrary/src/main/java/net/codejava/lib/CommonUtil.java
@@ -2,6 +2,6 @@ package net.codejava.lib;
 
 public class CommonUtil {
 	public static String getAppName() {
-		return "My Company App version 1.0.2";
+		return "My Company App version 1.0.1";
 	}
 }
