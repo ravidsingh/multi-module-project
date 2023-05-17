66b9ef09cb12576bd75038963674922f79230834
ravidsingh <125184625+ravidsingh@users.noreply.github.com>
2023-05-17 19:14:32 +0530

    Update AdminAppApplication.java

--- AdminApp/src/main/java/net/codejava/admin/AdminAppApplication.java
+++ AdminApp/src/main/java/net/codejava/admin/AdminAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdminAppApplication {
-	//add comments
+	//add comments  
 	public static void main(String[] args) {
 		SpringApplication.run(AdminAppApplication.class, args);
 	}
