b7b40d481288a6e6f0fa3de113e05db0199f93c0
ravidsingh <125184625+ravidsingh@users.noreply.github.com>
2023-06-09 23:31:08 +0530

    Revert "IQE-9"

--- AdminApp/src/main/java/net/codejava/admin/MainController.java
+++ AdminApp/src/main/java/net/codejava/admin/MainController.java
@@ -8,7 +8,7 @@ import net.codejava.lib.CommonUtil;
 
 @Controller
 public class MainController {
-	// controller for admin apps 
+	// controller for admin apps
 	@GetMapping("/admin")
 	public String viewHomePage(Model model) {
 		String appName = CommonUtil.getAppName();
